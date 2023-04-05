95e2815a89e54966a66c559b0562b60993d03309
mrbrunelli <matheus.brunelli@gmail.com>
2023-04-05 16:00:38 -0300

    add recipe router

---
+++ src/routes/recipe-routes.ts
@@ -0,0 +1,17 @@
+import { Request, Response, Router } from "express";
+import { RecipeService } from "../services/RecipeService";
+
+const recipeRouter = Router();
+
+const recipeService = new RecipeService();
+
+recipeRouter.post("/recipes", async (req: Request, res: Response) => {
+  try {
+    await recipeService.createNewRecipe(req.body);
+    return res.status(201).send();
+  } catch (error) {
+    return res.status(500).send("internal server error");
+  }
+});
+
+export { recipeRouter };
